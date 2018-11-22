6539a9b11dcfcc68d50fc6f23617a1a2ade0cfe1
Jason Wilcox <jasonwlcx@gmail.com>
2018-11-22 16:17:17 -0700

    recent updates to the flask_notebook project

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,17 +1,22 @@
 # services/users/project/config.py
 
+import os
+
 class BaseConfig:
     """Base Configuration"""
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'secret_key'
 
 class DevelopmentConfig:
     """Development Configuration"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class TestingConfig:
     """Testing Configuration"""
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 class ProductionConfig:
     """Production Configuration"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
